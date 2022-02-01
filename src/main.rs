1478256a80a82056492490048822ce1c60668f7f
Randy Ly <rly9842@hotmail.com>
2022-02-01 01:42:58 -0600

    Got the scrapper to get all the image links properly
    Issue with downloading

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,15 @@ fn main()
 {
     let args: Vec<String> = std::env::args().collect();
     let link: String = args[1].clone();
-    let depth: u8 = args[2].parse::<u8>().unwrap();
     let mut link_vec: Vec<String> = vec![];
 
     scrape::scrape(link, &mut link_vec);
 
-    // Print link_vec values
+    // Download Images
     for i in link_vec.iter()
     {
         println!("{}", i);
+        scrape::download_image(i);
     }
-
-    // Do something with the data?
 }
+
--- src/scrape.rs
+++ src/scrape.rs
@@ -17,9 +17,16 @@ pub async fn scrape(link: String, link_vec: &mut Vec<String>) -> Result<()> {
         .await?;
 
     Document::from(res.as_str())
-        .find(Name("a"))
-        .filter_map(|n| n.attr("href"))
+        .find(Name("img"))
+        .filter_map(|n| n.attr("src"))
         .for_each(|x| link_vec.push(x.to_string()));
         // .for_each(|x| println!("{}", x));
     return Ok(());
+}
+
+pub fn download_image(link: &str)
+{
+    // let mut resp = reqwest::get(link).expect("request failed");
+    // let mut out = std::fs::File::create(link).expect("failed to create file");
+    // std::io::copy(&mut resp, &mut out).expect("failed to copy content");
 }
